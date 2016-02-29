74a3fcd8e46708f910c9b858233da2f457846757
Rye Terrell <rterrell@zenoss.com>
2016-02-29 11:48:59 -0600

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.ZenMail"
-VERSION = "5.0.1"
+VERSION = "5.0.3dev"
 AUTHOR = "Zenoss"
 LICENSE = "All Rights Reserved"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
